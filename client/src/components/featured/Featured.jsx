a4a2df96691ea4e4919a613d8aecf78fb9fc6e14
mbayi-ios <91916741+mbayi-ios@users.noreply.github.com>
2023-03-27 15:10:39 +0300

    setting up home page

---
+++ client/src/components/featured/Featured.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import "./featured.scss"
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import { CircularProgressbar } from 'react-circular-progressbar';
+import "react-circular-progressbar/dist/styles.css"
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+
+const Featured = () => {
+  return (
+    <div className='featured'>
+      <div className='top'>
+        <h1 className="title">Total Revenue</h1>
+        <MoreVertIcon fontSize='small'/>
+      </div>
+      <div className="bottom">
+        <div className="featuredChart">
+              <CircularProgressbar value= {70} text={"70%"} strokeWidth={3}/>
+        </div>
+        <p className="title">Total Sales Made Today</p>
+        <p className="amount">$420</p>
+        <p className="description">Previous Transactions processing. Last Payments may not be included </p>
+        <div className="summary">
+            <div className="item">
+                <div className="itemTitle">Target</div>
+                <div className="itemResult positive">
+                    <KeyboardArrowUpIcon fontSize='small'/>
+                    <div className="resultAmount">$12.4k</div>
+                </div>
+            </div>
+            <div className="item">
+                <div className="itemTitle">Last Week</div>
+                <div className="itemResult negative">
+                    <KeyboardArrowDownIcon fontSize='small'/>
+                    <div className="resultAmount">$12.4k</div>
+                </div>
+            </div>
+            <div className="item">
+                <div className="itemTitle">Last Month</div>
+                <div className="itemResult">
+                    <KeyboardArrowUpIcon fontSize='small'/>
+                    <div className="resultAmount">$12.4k</div>
+                </div>
+            </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Featured
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
+import Chart from '../../components/chart/Chart'
+import Featured from '../../components/featured/Featured'
 import Navbar from '../../components/navbar/Navbar'
 import Sidebar from '../../components/sidebar/Sidebar'
+import Widget from '../../components/widgets/Widget'
 import "./home.scss"
 
 const Home = () => {
@@ -8,10 +11,18 @@ const Home = () => {
     <div className="home">
         <Sidebar/>
         <div className="homeContainer">
-         
-          <Navbar />
-        Home container
-          </div>
+        <Navbar />
+        <div className="widgets">
+          <Widget type="user" />
+          <Widget type="order" />
+          <Widget type = "earning" />
+          <Widget type="balance" />
+        </div>
+        <div className="charts">
+          <Featured />
+          <Chart />
+        </div>
+        </div>
     </div>
   )
 }
